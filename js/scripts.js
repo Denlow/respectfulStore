7e258ce1a39dd02ceecd0e0fe7a2ca330928dcca
Dave Denlow and Kevin Finley <dave@>
2016-10-24 10:31:49 -0700

    add basic html and js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$ (document).ready(function() {
+  $(.form).submit(function(){
+  var items = ["#item1", "#item2", "#item3", "item4"];
+  numbers.forEach(function(item){
+    items.toUpperCase();
+
+  });
+
+event.preventDefault();
+});
