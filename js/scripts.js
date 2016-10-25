680f6a9a2b19c69eab7060455d7b87c4ddccbd54
Dave Denlow and Kevin Finley <dave@>
2016-10-24 17:03:23 -0700

    add respectful store

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $ (document).ready(function() {
-  $(.form).submit(function(){
-  var items = ["#item1", "#item2", "#item3", "item4"];
-  numbers.forEach(function(item){
-    items.toUpperCase();
+  $("form#list").submit(function(){
+    debugger;
+    var item1 = $("input#item1").val();
+    var item2 = $("input#item2").val();
+    var item3 = $("input#item3").val();
+    var item4 = $("input#item4").val();
 
+    var newItems = [item1, item2, item3, item4];
+    var newArray = [];
+
+
+        $(".item1").text(newItems[0]);
+        $(".item2").text(newItems[1]);
+        $(".item3").text(newItems[2]);
+        $(".item4").text(newItems[3]);
+
+
+
+    event.preventDefault();
   });
-
-event.preventDefault();
 });
